775789e76649a0330b22f644b98e088c59fd75e0
Stéphane Diemer <stephane.diemer@ubicast.eu>
2015-12-12 20:26:38 +0100

    Fixed script name in init info.

--- mpc_lirc.py
+++ mpc_lirc.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 ### BEGIN INIT INFO
-# Provides:          mpc_lirc
+# Provides:          mpc-lirc
 # Required-Start:    $local_fs $network
 # Required-Stop:     $local_fs $network
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: mpc lirc daemon
-# Description:       starts the mpc_lirc daemon
+# Short-Description: mpc-lirc daemon
+# Description:       starts the mpc-lirc daemon
 ### END INIT INFO
 
 # This script should be located in /etc/init.d/mpc-lirc
